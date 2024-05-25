ff4ebfb78bfece456a7c4c8c58bdcf7785f7bd9c
mtugan <>
2024-05-25 22:41:05 +0000

    - stdvar does not need absolute bc squared - show current funding rates - fix a bug where rates where not showing for some assets - fix a bug where rates would be shown wrong due to async out of order

--- functions.py
+++ functions.py
@@ -12,9 +12,11 @@ import json
 from datetime import datetime
 
 
-async def make_request(session, url, headers, data):
+async def _async_coroutine(session, url, headers, data, callback=lambda _: _):
     async with session.post(url, headers=headers, json=data) as response:
-        return await response.text()
+        response = await response.text()
+        callback()
+        return response
 
 
 def fetch_hyperliquid_universe():
@@ -35,6 +37,23 @@ def fetch_hyperliquid_tokens(hl_universe_reponse):
     return [coin["name"] for coin in hl_universe_reponse["universe"]]
 
 
+def fetch_hyperliquid_universe_ctx():
+    url = "https://api.hyperliquid.xyz/info"
+    header_data = {"Content-Type": "application/json"}
+    request_data = {"type": "metaAndAssetCtxs"}
+    return json.loads(requests.post(url, headers=header_data, json=request_data).text)
+
+
+def fetch_hyperliquid_current_funding(hl_universe_ctx_response):
+    if not hl_universe_ctx_response:
+        raise Exception(
+            "Something went wrong with fetching data from Hyperliquid, error\n"
+            + hl_universe_ctx_response.text
+        )
+        sys.exit(0)
+    return [rates["funding"] for rates in hl_universe_ctx_response]
+
+
 async def _fetch_hyperliquid_historical_funding(coins, start_times, end_times=[]):
     if len(coins) != len(start_times):
         raise Exception("Coins / times must be same len")
@@ -46,7 +65,19 @@ async def _fetch_hyperliquid_historical_funding(coins, start_times, end_times=[]
         timestamp = datetime.now().timestamp()
         end_times = [timestamp for _ in start_times]
 
+    # Urls
+
     url = "https://api.hyperliquid.xyz/info"
+    header = {"Content-Type": "application/json"}
+
+    # We need this callback to so we can later correct if HL doesn't answer properly
+
+    responses = None
+
+    response_order = []
+
+    def request_callback(coin):
+        return lambda: response_order.append(coin)
 
     async with aiohttp.ClientSession() as session:
         tasks = []
@@ -59,23 +90,59 @@ async def _fetch_hyperliquid_historical_funding(coins, start_times, end_times=[]
 
             tasks.append(
                 asyncio.ensure_future(
-                    make_request(
+                    _async_coroutine(
                         session,
                         url,
-                        {"Content-Type": "application/json"},
+                        header,
                         {
                             "type": "fundingHistory",
                             "coin": coins[i],
                             "startTime": int(start_times[i] * 1000),
                             "endTime": int(end_times[i] * 1000),
                         },
+                        request_callback(
+                            coins[i]
+                        ),  # Returns the callback function here
                     )
                 )
             )
 
         responses = await asyncio.gather(*tasks)
 
-    return [json.loads(response) for response in responses]
+    responses = [json.loads(response) for response in responses]
+
+    # We need to sort the coins
+    funding_data = [None] * len(coins)
+
+    # We need a key map for it
+    kmap = {value: index for index, value in enumerate(coins)}
+
+    # Now we need to build the array, and also HL sometimes doesn't return values properly
+    # so we need to refetch
+
+    responses_index = 0
+
+    while responses_index < len(responses):
+        token_data = responses[responses_index]
+
+        if token_data:
+            funding_data[kmap[token_data[0]["coin"]]] = token_data
+            responses_index += 1
+        else:
+            responses[responses_index] = json.loads(
+                requests.post(
+                    url,
+                    headers=header,
+                    json={
+                        "type": "fundingHistory",
+                        "coin": response_order[responses_index],
+                        "startTime": int(start_times[i] * 1000),
+                        "endTime": int(end_times[i] * 1000),
+                    },
+                ).text
+            )
+
+    return funding_data
 
 
 def fetch_hyperliquid_historical_funding(coins, start_times, end_times=[]):
--- funding_heatmap.py
+++ funding_heatmap.py
@@ -14,7 +14,8 @@ from datetime import datetime
 from functions import (
     fetch_hyperliquid_historical_funding,
     fetch_hyperliquid_tokens,
-    fetch_hyperliquid_universe,
+    fetch_hyperliquid_universe_ctx,
+    fetch_hyperliquid_current_funding,
 )
 
 
@@ -22,95 +23,130 @@ def format_percentage(value, pos):
     return f"{value:.5%}"
 
 
-def create_heatmap(
-    historical_funding_data, tokens, start_time, max_tokens_per_window=25
-):
-    # Calculate the standard deviation of absolute funding rates
+def create_heatmap(historical_funding_data, tokens, max_tokens_per_window=25):
+    # We will need multiple windows because we can't fit all tokens nicely on one
+    figures = []
+
+    num_windows = math.ceil(len(tokens) / max_tokens_per_window)
+
+    # Calculate the standard deviation of funding rates
     all_funding_rates = [
-        abs(float(t["fundingRate"]))
+        float(t["fundingRate"])
         for token_data in historical_funding_data
         if token_data
         for t in token_data
     ]
 
-    figures = []
-
-    num_windows = math.ceil(len(tokens) / max_tokens_per_window)
-
     stdev = np.std(all_funding_rates)
 
+    # Start building the heatmaps
+
     for window in range(num_windows):
+        # Start index of individual heatmap
         start_index = window * max_tokens_per_window
 
+        # EXCLUSIVE end index of individual heatmap
         end_index = min((window + 1) * max_tokens_per_window, len(tokens))
 
-        index = start_index
+        # The tokens are our y ticks
+        y_ticks = tokens[start_index:end_index]
 
-        current_tokens = tokens[start_index:end_index]
+        # Funding rates our heat
+        heat_values = []
 
-        y_ticks = current_tokens
-
-        data = []
-
+        # X ticks will is unix timestamp
         x_ticks = []
 
-        while index < end_index:
-            token_data = historical_funding_data[index]
-            if token_data:
-                data.append([float(t["fundingRate"]) for t in token_data])
-                x_ticks = [datetime.fromtimestamp(t["time"] / 1000) for t in token_data]
-                index += 1
-            else:
-                historical_funding_data[index] = fetch_hyperliquid_historical_funding(
-                    [tokens[index]], [start_time]
-                )[0]
+        # Sometimes hyperliquid returns something malformed or nothing we just fetch for those again
+        for token_data in historical_funding_data[start_index:end_index]:
+            heat_values.append([float(t["fundingRate"]) for t in token_data])
+            x_ticks = [datetime.fromtimestamp(t["time"] / 1000) for t in token_data]
 
+        # Get new subplot and add figure
         fig, ax = plt.subplots(figsize=(24, 12))
         figures.append(fig)  # Store the figure reference
 
-        im = ax.imshow(data, cmap="RdYlGn", vmin=-stdev, vmax=stdev, aspect="auto")
+        # Create heatmap
+        im = ax.imshow(
+            heat_values, cmap="RdYlGn", vmin=-stdev, vmax=stdev, aspect="auto"
+        )
 
+        # Set x axis ticks
         ax.set_xticks(range(len(x_ticks)))
         ax.set_xticklabels(
-            [date.strftime("%H:00 %d %b") for date in x_ticks],
+            [date.strftime("%H:%M %d %b") for date in x_ticks],
             rotation=45,
             ha="right",
             fontsize=8,
         )
 
+        # Set y axis ticks
         ax.set_yticks(range(len(y_ticks)))
         ax.set_yticklabels(y_ticks)
 
+        # Set colour style
         cbar = ax.figure.colorbar(im, ax=ax, format=FuncFormatter(format_percentage))
+
+        # y label
         cbar.ax.set_ylabel("Funding Rate", rotation=-90, va="bottom")
 
+        # Title
         ax.set_title(f"Hyperliquid Funding Rates - Window {window + 1}")
+
+        # Layout
         fig.tight_layout()
 
+        # For each heatmap box style & add text
         for i in range(len(y_ticks)):
             for j in range(len(x_ticks)):
-                if i < len(data) and j < len(data[i]):
+                if i < len(heat_values) and j < len(heat_values[i]):
                     text = ax.text(
                         j,
                         i,
-                        f"{data[i][j]:.4%}",
+                        f"{heat_values[i][j]:.5%}",
                         ha="center",
                         va="center",
                         color="black",
                         fontsize=6,
                     )
 
+        # Save fig move onto next
         plt.savefig("funding_heatmap_" + f"{window}" + ".png")
 
     plt.show()  # Display all the heatmaps at once
 
 
-tokens = fetch_hyperliquid_tokens(fetch_hyperliquid_universe())
+# Fetch market data
+hyperliquid_universe_ctx = fetch_hyperliquid_universe_ctx()
 
-start_time = datetime.now().timestamp() - 24 * 60 * 60
+# Assign tokens and current funding
+
+tokens = fetch_hyperliquid_tokens(hyperliquid_universe_ctx[0])
+
+hyperliquid_current_funding = fetch_hyperliquid_current_funding(
+    hyperliquid_universe_ctx[1]
+)
+
+# Set start time
+
+current_time = datetime.now().timestamp()
+
+start_time = current_time - 24 * 60 * 60
 
 historical_funding_data = fetch_hyperliquid_historical_funding(
     tokens, [start_time for _ in tokens]
 )
 
-create_heatmap(historical_funding_data, tokens, start_time)
+# Add to the historical funding data current values
+
+for i in range(len(historical_funding_data)):
+    # We don't need premium
+    historical_funding_data[i].append(
+        {
+            "coin": historical_funding_data[i][0]["coin"],
+            "fundingRate": hyperliquid_current_funding[i],
+            "time": int(current_time * 1000),
+        }
+    )
+
+create_heatmap(historical_funding_data, tokens)
